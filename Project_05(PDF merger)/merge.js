929f0f5b0cfd2f765c490fc0e933b55d56944ea4
Sundaram pandey <115979242+Sundarampandey19@users.noreply.github.com>
2023-06-10 17:18:35 +0530

    Update merge.js
    removed unnecessary comments

--- Project_05(PDF merger)/merge.js
+++ Project_05(PDF merger)/merge.js
@@ -9,17 +9,7 @@ const mergePdfs =  async (P1,P2) => {
   let d = new Date().getTime()
   await merger.save(`public/${d}.pdf`); //save under given name and reset the internal document
   return d
-  // await merger.add('1.pdf');  //merge all pages. parameter is the path to file and filename.
-  // await merger.add('2.pdf');  //merge all pages. parameter is the path to file and filename.
-  //   await merger.add('pdf2.pdf', 2); // merge only page 2
-  //   await merger.add('pdf2.pdf', [1, 3]); // merge the pages 1 and 3
-  //   await merger.add('pdf2.pdf', '4, 7, 8'); // merge the pages 4, 7 and 8
-  //   await merger.add('pdf3.pdf', '3 to 5'); //merge pages 3 to 5 (3,4,5)
-  //   await merger.add('pdf3.pdf', '3-5'); //merge pages 3 to 5 (3,4,5)
-  
-  // Export the merged PDF as a nodejs Buffer
-  // const mergedPdfBuffer = await merger.saveAsBuffer();
-  // fs.writeSync('merged.pdf', mergedPdfBuffer);
+ 
 }
 
 module.exports= {mergePdfs}
